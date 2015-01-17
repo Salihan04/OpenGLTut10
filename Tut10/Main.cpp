82d9f0dfec21a4f7b80a47d4a27845ef801afcf9
Salihan04 <salihan04@gmail.com>
2015-01-17 15:51:09 +0800

    Colors done

--- Tut10/Main.cpp
+++ Tut10/Main.cpp
@@ -4,10 +4,36 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
+#include "ShaderLoader.h"
 #include "Camera.h"
 
 using namespace std;
+
+//Function prototypes
+void createCubeVertexBuffer();
+void renderScene();
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void Do_Movement();
+
+GLuint programID, lightShaderProgramID, cubeVertexBuffer, lightVAO;
+
+// Object properties
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+// Camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+bool keys[1024];
+GLfloat lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
 
 int main(int argc, char **argv) {
 	//initialise GLFW
@@ -31,6 +57,14 @@ int main(int argc, char **argv) {
 	glfwMakeContextCurrent(window);
 	glfwSetWindowPos(window, 400, 200);
 
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// Options
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	//initialize GLEW
 	glewExperimental = true;		// Needed for core profile
 	if (glewInit() != GLEW_OK) {
@@ -38,18 +72,195 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
+	//Define the viewport dimensions
+	glViewport(0, 0, 700, 500);
+
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+
+	//need to be after glewInit(), otherwise got error
+	programID = LoadShaders("VertexShader.vertexshader", "FragmentShader.fragmentshader");
+	lightShaderProgramID = LoadShaders("VertexShaderLamp.vertexshader", "FragmentShaderLamp.fragmentshader");
+
+	createCubeVertexBuffer();
 
 	while (!glfwWindowShouldClose(window))
 	{
+		// Set frame time
+		GLfloat currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
 			glfwSetWindowShouldClose(window, GL_TRUE);
 
+		renderScene();
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+		Do_Movement();
 	}
 
 	glfwTerminate();
 
 	return 0;
+}
+
+void renderScene() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//use the shader
+	glUseProgram(programID);
+
+	//set uniform objectColor in Fragment Shader
+	GLint objectColorLoc = glGetUniformLocation(programID, "objectColor");
+	glUniform3f(objectColorLoc, 1.0f, 0.5f, 0.31f);
+
+	// Create camera transformations
+	glm::mat4 view;
+	view = camera.GetViewMatrix();
+	glm::mat4 projection;
+	projection = glm::perspective(camera.Zoom, 700.0f / 500.0f, 0.1f, 1000.0f);
+	// Get the uniform locations
+	GLint modelLoc = glGetUniformLocation(programID, "model");
+	GLint viewLoc = glGetUniformLocation(programID, "view");
+	GLint projLoc = glGetUniformLocation(programID, "projection");
+	// Pass the matrices to the shader
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVertexBuffer);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glm::mat4 model;
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	//lamp oject
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+	// We only need to bind to our VBO, the VBO's data is already what we want.
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVertexBuffer);
+	// Set our vertex attributes (only position data for our lamp)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+
+	//use the shader for the lamp
+	glUseProgram(lightShaderProgramID);
+
+	// Get location objects for the matrices on our lamp shader (these could be different on a different shader)
+	modelLoc = glGetUniformLocation(lightShaderProgramID, "model");
+	viewLoc = glGetUniformLocation(lightShaderProgramID, "view");
+	projLoc = glGetUniformLocation(lightShaderProgramID, "projection");
+	// Set matrices
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+	model = glm::mat4();
+	model = glm::translate(model, lightPos);
+	model = glm::scale(model, glm::vec3(0.2f)); // Make it a smaller cube
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	// Draw our light object (using light's vertex attributes)
+	glBindVertexArray(lightVAO);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+
+	//Disable vertex atrributes
+	glDisableVertexAttribArray(0);
+}
+
+void createCubeVertexBuffer() {
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, 0.5f, -0.5f,
+		0.5f, 0.5f, -0.5f,
+		-0.5f, 0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+
+		-0.5f, -0.5f, 0.5f,
+		0.5f, -0.5f, 0.5f,
+		0.5f, 0.5f, 0.5f,
+		0.5f, 0.5f, 0.5f,
+		-0.5f, 0.5f, 0.5f,
+		-0.5f, -0.5f, 0.5f,
+
+		-0.5f, 0.5f, 0.5f,
+		-0.5f, 0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, 0.5f,
+		-0.5f, 0.5f, 0.5f,
+
+		0.5f, 0.5f, 0.5f,
+		0.5f, 0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, 0.5f,
+		0.5f, 0.5f, 0.5f,
+
+		-0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, 0.5f,
+		0.5f, -0.5f, 0.5f,
+		-0.5f, -0.5f, 0.5f,
+		-0.5f, -0.5f, -0.5f,
+
+		-0.5f, 0.5f, -0.5f,
+		0.5f, 0.5f, -0.5f,
+		0.5f, 0.5f, 0.5f,
+		0.5f, 0.5f, 0.5f,
+		-0.5f, 0.5f, 0.5f,
+		-0.5f, 0.5f, -0.5f
+	};
+
+	glGenBuffers(1, &cubeVertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+// Moves/alters the camera positions based on user input
+void Do_Movement()
+{
+	// Camera controls
+	if (keys[GLFW_KEY_W])
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (keys[GLFW_KEY_S])
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (keys[GLFW_KEY_A])
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (keys[GLFW_KEY_D])
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (action == GLFW_PRESS)
+		keys[key] = true;
+	else if (action == GLFW_RELEASE)
+		keys[key] = false;
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
 }
