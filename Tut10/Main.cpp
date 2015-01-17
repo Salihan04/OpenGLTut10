d4175aaf205e814787e18af4a5ef86a0a98dd43d
Salihan04 <salihan04@gmail.com>
2015-01-17 13:49:12 +0800

    Added Camera class

--- Tut10/Main.cpp
+++ Tut10/Main.cpp
@@ -1,8 +1,55 @@
+#include <stdio.h>
 #include <stdlib.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GL/freeglut.h>
+
+#include "Camera.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
-	 
+	//initialise GLFW
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to initialise GLFW\n");
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, 0);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow *window = glfwCreateWindow(700, 500, "OpenGLTut10 - Lighting", NULL, NULL);
+	if (window == NULL){
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetWindowPos(window, 400, 200);
+
+	//initialize GLEW
+	glewExperimental = true;		// Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, GL_TRUE);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
 	return 0;
 }
